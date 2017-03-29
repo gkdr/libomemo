f7ea4b9ba1de7b4b7c26d6e3c6c9d675086905f5
Bob Mottram <bob@freedombone.net>
2017-03-29 15:00:10 +0100

    Set license and source headers to GPL3
    This uses the exact same license text as used by libsignal-protocol-c and does not include 'or later' within the headers #11

--- src/libomemo.h
+++ src/libomemo.h
@@ -1,7 +1,16 @@
 /**
- * LIBOMEMO 0.5.0
+ * libomemo
+ * Copyright (C) 2017
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
-
 
 #pragma once
 
